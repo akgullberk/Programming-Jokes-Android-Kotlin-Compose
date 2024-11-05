99371fbb28d14c591a0dfe5c309d46aa5bc625fd
Berk Akgül <berkak_gul51@hotmail.com>
2024-11-05 16:12:37 +0300

    joke model

---
+++ app/src/main/java/com/example/programmingjokes/model/Joke.kt
@@ -0,0 +1,24 @@
+package com.example.programmingjokes.model
+
+class Joke(
+    val error : Boolean,
+    val category : String,
+    val type : String,
+    val setup : String? = null,
+    val delivery : String? = null,
+    val joke : String? = null,
+    val flags : Flags,
+    val id : Long,
+    val safe : Boolean,
+    val lang : String
+
+)
+
+class Flags(
+    val nsfw : Boolean,
+    val religious : Boolean,
+    val political : Boolean,
+    val racist : Boolean,
+    val sexist : Boolean,
+    val explicit : Boolean
+)
